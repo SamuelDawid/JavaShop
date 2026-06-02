1ffc1365d378860b11f558b3c898ab8af54fd0b8
Samuel K <samuelkrzyszpien@gmail.com>
2026-06-02 13:52:48 +0200

    Delete hibernate + JPA, Create InMemory repo, implemented ProductManager

--- JavaShop/src/main/java/org/javashop/service/ProductManager.java
+++ JavaShop/src/main/java/org/javashop/service/ProductManager.java
@@ -1,4 +1,23 @@
 package org.javashop.service;
 
+
+import lombok.NonNull;
+import org.javashop.models.Electronics;
+import org.javashop.repo.InMemoryProductRepository;
+
 public class ProductManager {
+    private final InMemoryProductRepository productsRepository;
+
+    public ProductManager(InMemoryProductRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+    public void addProduct(@NonNull Electronics product){
+            productsRepository.save(product);
+    }
+    public void modify(String id,@NonNull Electronics product){
+        productsRepository.update(id, product);
+    }
+    public boolean delete(String id){
+        return productsRepository.delete(id);
+    }
 }
---
+++ JavaShop/src/test/java/org/javashop/service/ProductManagerTest.java
@@ -0,0 +1,20 @@
+package org.javashop.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
