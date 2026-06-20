731e0c54ddf6c37c43e19ab6b5cf2cdd559660c4
Samuel K <samuelkrzyszpien@gmail.com>
2026-06-20 10:05:40 +0200

    chore: stop tracking .dead folder

--- JavaShop/src/main/java/org/javashop/App.java
+++ JavaShop/src/main/java/org/javashop/App.java
@@ -21,32 +21,31 @@ import org.javashop.service.ShopCLI;
 import java.math.BigDecimal;
 import java.util.List;
 
-public class App
-{
-    public static void main( String[] args ) throws InterruptedException {
+public class App {
+    public static void main(String[] args) throws InterruptedException {
 
         InMemoryProductRepository repository = new InMemoryProductRepository();
         InMemoryVoucherRepository voucherRepository = new InMemoryVoucherRepository();
         ProductManager manager = new ProductManager(repository);
         OrderProcessor orderProcessor = new OrderProcessor(manager);
         DiscountService discountService = new DiscountService(voucherRepository);
-        Account account = new Account("123","Samuel K", AccountType.NORMAL);
+        Account account = new Account("123", "Samuel K", AccountType.NORMAL);
         Cart cart = new Cart(account);
         //regionProducts
         manager.addAllProducts(List.of(
-          new Computer("PC-1", "Gaming Beast", new BigDecimal("3999.99"), 5,
-                CPU.AMD, GPU.NVIDIA, RAM.GB32),
-         new Computer("PC-2", "Office Pro", new BigDecimal("1499.99"), 10,
-                CPU.INTEL, GPU.INTEL, RAM.GB16),
-         new SmartPhone("PH-1", "iPhone 15", new BigDecimal("4299.99"), 8,
-                BATTERY.mAh_4000, Colour.BLACK),
-         new SmartPhone("PH-2", "Samsung S24", new BigDecimal("3199.99"), 3,
-                BATTERY.mAh_5000, Colour.GRAY),
-        new SmartPhone("PH-3", "Xiaomi 13", new BigDecimal("999.99"), 15,
-                BATTERY.mAh_5000, Colour.GREEN)));
+                new Computer("PC-1", "Gaming Beast", new BigDecimal("3999.99"), 5,
+                        CPU.AMD, GPU.NVIDIA, RAM.GB32),
+                new Computer("PC-2", "Office Pro", new BigDecimal("1499.99"), 10,
+                        CPU.INTEL, GPU.INTEL, RAM.GB16),
+                new SmartPhone("PH-1", "iPhone 15", new BigDecimal("4299.99"), 8,
+                        BATTERY.mAh_4000, Colour.BLACK),
+                new SmartPhone("PH-2", "Samsung S24", new BigDecimal("3199.99"), 3,
+                        BATTERY.mAh_5000, Colour.GRAY),
+                new SmartPhone("PH-3", "Xiaomi 13", new BigDecimal("999.99"), 15,
+                        BATTERY.mAh_5000, Colour.GREEN)));
 
         //endregion
-        new ShopCLI(manager,cart,orderProcessor,discountService,account).start();
+        new ShopCLI(manager, cart, orderProcessor, discountService, account).start();
 
 
     }
